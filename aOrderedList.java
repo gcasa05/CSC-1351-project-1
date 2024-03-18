961c7bc70e8360b0f995ac036e97f3370ee28f9a
gcasa05 <98133585+gcasa05@users.noreply.github.com>
2024-03-17 22:00:25 -0500

    Update aOrderedList.java

--- aOrderedList.java
+++ aOrderedList.java
@@ -1,4 +1,25 @@
 
 public class aOrderedList {
+	final int SIZEINCREMENTS = 20; 
+	@SuppressWarnings("unused")
+	private car[] oList; 
+	private int listSize; 
+	private int numObjects;
+	
+	public aOrderedList() {
+		this.numObjects = 0;
+		this.listSize = SIZEINCREMENTS;
+		this.oList = new car[SIZEINCREMENTS];
+		
+		//int nums[]= new int[size];
+	}
+	
+	public void add(car newCar) {
+		oList.add(newCar);
+	}
+	 public String toString() {
+	 		return "[" + super.toString() + "]";
+	  }
 
+	
 }
