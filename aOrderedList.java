431a5bc0394a3642721cd5f0d73ecaa30c1f1dae
gcasa05 <98133585+gcasa05@users.noreply.github.com>
2024-03-17 23:28:17 -0500

    Update aOrderedList.java

--- aOrderedList.java
+++ aOrderedList.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 
 public class aOrderedList {
 	final int SIZEINCREMENTS = 20; 
-	@SuppressWarnings("unused")
+	
 	private car[] oList; 
 	private int listSize; 
 	private int numObjects;
@@ -14,8 +15,44 @@ public class aOrderedList {
 		//int nums[]= new int[size];
 	}
 	
+	public int size() {
+		return numObjects;
+	}
+	public car get(int index) {
+		return oList[index];
+	}
+	public boolean isEmpty() {
+	 if(oList == null) {
+		 return true;
+	 } else {
+		 return false;
+	 }
+			
+		
+	}
+	public void remove(int index) {
+		car[] temp = new car[oList.length];//I don't know format
+	    for(int i = 0; i<oList.length; i++) {
+	        if(i != index)
+	        {
+	            temp[i] = oList[i];
+	           
+	        }
+	    }
+	    temp[oList.length-1] = null;
+	    oList = temp;
+	}
+	
+	
+	
 	public void add(car newCar) {
-		oList.add(newCar);
+		oList[numObjects] = newCar;
+		numObjects ++; 
+		if(numObjects == oList.length) {
+			car[] newVal = Arrays.copyOf(oList, 2 * oList.length);
+			oList = newVal;
+
+		}
 	}
 	 public String toString() {
 	 		return "[" + super.toString() + "]";
